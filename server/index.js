f359308670c268614a35183fb79be73f346a4148
alperencinar <alperencnrr@gmail.com>
2024-10-18 14:23:45 +0300

    v1.8
    serves index js updated with mongoosee

--- server/index.js
+++ server/index.js
@@ -3,29 +3,46 @@ const express = require('express');
 const chalk = require('chalk');
 const cors = require('cors');
 const helmet = require('helmet');
+const mongoose = require('mongoose');
 
 const keys = require('./config/keys');
 const routes = require('./routes');
 const socket = require('./socket');
-const setupDB = require('./utils/db');
 
 const { port } = keys;
 const app = express();
 
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   helmet({
     contentSecurityPolicy: false,
-    frameguard: true
+    frameguard: true,
   })
 );
 app.use(cors());
 
-setupDB();
+// MongoDB Connection Using Mongoose
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(chalk.green('✓ Connected to MongoDB Atlas'));
+  })
+  .catch((err) => {
+    console.error(chalk.red('✗ MongoDB connection error:'), err);
+  });
+
+// Passport configuration (for authentication)
 require('./config/passport')(app);
+
+// Application routes
 app.use(routes);
 
+// Start the server
 const server = app.listen(port, () => {
   console.log(
     `${chalk.green('✓')} ${chalk.blue(
@@ -34,4 +51,5 @@ const server = app.listen(port, () => {
   );
 });
 
+// Socket setup
 socket(server);
